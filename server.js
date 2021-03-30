0d94a569a99b54940b520e588690f1ac3953b5a1
MissNG <65439386+MissNG-Git@users.noreply.github.com>
2021-03-31 02:56:13 +1100

    set up server

---
+++ server.js
@@ -0,0 +1,34 @@
+// Require packages
+const express = require("express");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+
+// Setup Express App
+const app = express();
+const PORT = process.env.PORT || 8080;
+// Data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// morgan.format(name, format)
+app.use(morgan("dev"));
+
+// Use static directory for assets
+app.use(express.static("public"));
+
+// Invoke Routes
+app.use(require("./routes/api-routes.js"));
+app.use(require("./routes/html-routes.js"));
+
+// Setup Mongoose Atlas environment
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workitDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server listening on: http://localhost:${PORT}!`);
+});
