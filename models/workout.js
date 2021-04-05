320be7fe34905a53f6eb89d9d2d53cbd60b9d9b7
MissNG <65439386+MissNG-Git@users.noreply.github.com>
2021-04-05 17:07:47 +1000

    set up models

---
+++ models/workout.js
@@ -0,0 +1,63 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const opts = { toJSON: { virtuals: true } };
+
+const workoutSchema = new Schema(
+  {
+    day: {
+      type: Date,
+      default: Date.now
+    },
+    exercises: [
+      {
+        type: {
+          type: String,
+          trim: true,
+          required: "Enter an exercise type"
+        },
+        name: {
+          type: String,
+          trim: true,
+          required: "Enter an exercise name"
+        },
+        distance: {
+          type: Number,
+          default: 0
+        },
+        duration: {
+          type: Number,
+          default: 0,
+          required: "Enter an exercise duration (in mins.)"
+        },
+        reps: {
+          type: Number,
+          default: 0
+        },
+        sets: {
+          type: Number,
+          default: 0
+        },
+        weight: {
+          type: Number,
+          default: 0
+        }
+      }
+    ]
+  },
+  // Include Mongoose virtuals 
+  opts
+);
+
+// Create virtual property "totalDuration" from exercises
+workoutSchema.virtual("totalDuration").get(() => {
+    // "reduce" exercises array to sum of durations
+    return this.exercises.reduce((total, exercise) => {
+        return total + exercise.duration;
+    }, 0);
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
